67eef2d65c0b5cd062ac646d3d39b1045d2cfcc2
alexiadltg <a22aledelgar@inspedralbes.cat>
2022-10-25 12:46:10 +0200

    add google maps integration

--- app/src/main/java/org/deplastic/Deplastic/MainActivity.java
+++ app/src/main/java/org/deplastic/Deplastic/MainActivity.java
@@ -1,14 +1,37 @@
 package org.deplastic.Deplastic;
 
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //TODO import variables from BD
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        LatLng barcelona = new LatLng(-30.852, 150.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.addMarker(new MarkerOptions().position(barcelona).title("Marker in Barcelona"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
