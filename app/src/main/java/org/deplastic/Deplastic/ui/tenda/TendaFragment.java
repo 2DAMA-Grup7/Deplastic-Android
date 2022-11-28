9d81e6fcb9e600c2956eacd0daa37528a386d073
alexiadltg <a22aledelgar@inspedralbes.cat>
2022-11-28 09:22:29 +0100

    none

--- app/src/main/java/org/deplastic/Deplastic/ui/tenda/TendaFragment.java
+++ app/src/main/java/org/deplastic/Deplastic/ui/tenda/TendaFragment.java
@@ -7,16 +7,34 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.deplastic.Deplastic.MyRecyclerViewAdapter;
 import org.deplastic.Deplastic.R;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class TendaFragment extends Fragment {
+import java.util.ArrayList;
+
+public class TendaFragment extends Fragment implements MyRecyclerViewAdapter.ItemClickListener {
 
     private TendaViewModel mViewModel;
+
+    MyRecyclerViewAdapter adapter;
+
 
     public static TendaFragment newInstance() {
         return new TendaFragment();
@@ -25,7 +43,37 @@ public class TendaFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tenda, container, false);
+        View view = inflater.inflate(R.layout.fragment_tenda, container, false);
+
+        // data to populate the RecyclerView with
+
+        ArrayList<String> productNames = new ArrayList<>();
+
+        // Get markers from api rest and show them all
+        RequestQueue queue = Volley.newRequestQueue(requireActivity().getApplicationContext());
+        String url = "https://deplastic.netlify.app/.netlify/functions/api/products";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject product = response.getJSONObject(i);
+                            productNames.add(product.getString("nom"));
+                        }
+                        // set up the RecyclerView
+                        RecyclerView recyclerView = view.findViewById(R.id.Products);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                        adapter = new MyRecyclerViewAdapter(getContext(), productNames);
+                        adapter.setClickListener(this);
+                        recyclerView.setAdapter(adapter);
+
+                    } catch (JSONException e) { e.printStackTrace(); }
+                }, error -> Toast.makeText(requireActivity().getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show());
+        // Add the request to the RequestQueue.
+        queue.add(jsonArrayRequest);
+
+
+
+        return view;
     }
 
     @Override
@@ -35,4 +83,8 @@ public class TendaFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
 }
