d11528d28a7286b3a7a43487fc8ede02a9344e9f
a19marsbasgra <a19marsbasgra@inspedralbes.cat>
2022-11-30 12:44:50 +0100

    Tenda compra i envia a BD

--- app/src/main/java/org/deplastic/Deplastic/ui/tenda/producte/ProductFragment.java
+++ app/src/main/java/org/deplastic/Deplastic/ui/tenda/producte/ProductFragment.java
@@ -1,19 +1,8 @@
 package org.deplastic.Deplastic.ui.tenda.producte;
 
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +10,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -30,14 +24,9 @@ import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
-import org.deplastic.Deplastic.Adapter;
 import org.deplastic.Deplastic.R;
-import org.deplastic.Deplastic.databinding.FragmentConfigBinding;
-import org.deplastic.Deplastic.ui.config.balance.BalanceFragment;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.Objects;
 
 public class ProductFragment extends Fragment {
     static int prodnum;
@@ -51,16 +40,20 @@ public class ProductFragment extends Fragment {
         return new ProductFragment(prodnum);
     }
 
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        ProductViewModel productViewModel =
-                new ViewModelProvider(this).get(ProductViewModel.class);
+        ProductViewModel productViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_product, container, false);
 
-        //    TextView prodName  = getView().findViewById(R.id.NomProd);
-        //  TextView prodDesc  = getView().findViewById(R.id.ProdDescription);
-        //  TextView pointCost  = getView().findViewById(R.id.PointCost);
-        //  ImageView imatge = getView().findViewById(R.id.imatgeProd);
+
+        /*
+        TextView prodName  = getView().findViewById(R.id.NomProd);
+        TextView prodDesc  = getView().findViewById(R.id.ProdDescription);
+        TextView pointCost  = getView().findViewById(R.id.PointCost);
+        ImageView imatge = getView().findViewById(R.id.imatgeProd);
+        */
 
         RequestQueue queue = Volley.newRequestQueue(requireActivity().getApplicationContext());
         String url = "https://deplastic.netlify.app/.netlify/functions/api/products";
@@ -89,11 +82,11 @@ public class ProductFragment extends Fragment {
         // Add the request to the RequestQueue.
         queue.add(jsonArrayRequest);
 
-      Button buyButton = getView().findViewById(R.id.buyButton);
+        Button buyButton = view.findViewById(R.id.buyButton);
 
         JSONObject compra = new JSONObject();
-        SharedPreferences sp = getActivity().getSharedPreferences("email",Context.MODE_PRIVATE);
-        String emailVal = sp.getString("email","");
+        SharedPreferences sp = requireActivity().getSharedPreferences("email", Context.MODE_PRIVATE);
+        String emailVal = sp.getString("email", "");
         int prodId = prodnum;
 
         try {
@@ -102,7 +95,6 @@ public class ProductFragment extends Fragment {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
 
         buyButton.setOnClickListener(v -> {
             String newurl = "https://deplastic.netlify.app/.netlify/functions/api/receipt";
@@ -118,8 +110,7 @@ public class ProductFragment extends Fragment {
             queue.add(jsonObjectRequest);
         });
 
-
-        return inflater.inflate(R.layout.fragment_product, container, false);
+        return view;
     }
 
 }
